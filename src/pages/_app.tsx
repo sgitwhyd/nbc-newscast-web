409fa3f6fdb73763d2f10b3e6ab37f590d0ebdcd
Sigit Wahyudi <sigitwahyu.mutu@gmail.com>
2023-10-16 11:01:56 +0700

    feat: adding nProgress and wrap component with root layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,21 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import "nprogress/nprogress.css";
+import type { AppProps } from "next/app";
+import RootLayout from "@/components/layout/root-layout";
+import nProgress from "nprogress";
+import { Router } from "next/router";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	nProgress.configure({
+		showSpinner: false,
+	});
+
+	Router.events.on("routeChangeStart", nProgress.start);
+	Router.events.on("routeChangeError", nProgress.done);
+	Router.events.on("routeChangeComplete", nProgress.done);
+	return (
+		<RootLayout>
+			<Component {...pageProps} />
+		</RootLayout>
+	);
 }
