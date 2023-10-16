a9389def48a8d6dd996557d7261d4c417c0d133c
Sigit Wahyudi <sigitwahyu.mutu@gmail.com>
2023-10-16 11:00:40 +0700

    feat(news): create several types for news response api

---
+++ src/types/news/index.ts
@@ -0,0 +1,35 @@
+export interface INews {
+	id: string;
+	name: string;
+	description: string;
+	url: string;
+	category: string;
+	language: string;
+	country: string;
+}
+
+export interface IArticle {
+	source: {
+		id: string;
+		name: string;
+	};
+	author: string;
+	title: string;
+	description: string;
+	url: string;
+	urlToImage: string;
+	publishedAt: string;
+	content: string;
+}
+
+export interface INewsApiResponse {
+	status: string;
+	totalResults: number;
+	articles: IArticle;
+}
+
+export interface IErrorResponse {
+	code: string;
+	message: string;
+	status: number;
+}
